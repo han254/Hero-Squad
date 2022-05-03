3c0adbe7c1bdec0baee9671a47a7c7927fe72c9f
han254 <peternjenga1706@gmail.com>
2022-05-03 14:23:06 +0300

    initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,37 @@
+import models.Hero;
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+import static spark.Spark.staticFileLocation;
+
+public class App {
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        Hero.setUpFirstHero();
+        Hero.setUpSecondHero();
+
+
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero-form",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero",(req, res) ->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getAllInstances();
+            model.put("hero",hero);
+            return new ModelAndView(model, "hero.hbs");
+        }, new HandlebarsTemplateEngine());
+    }
+
+}
