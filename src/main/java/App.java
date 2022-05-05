d73682cfb9a199b19b2e1394cd5140c22bce6fe5
han254 <peternjenga1706@gmail.com>
2022-05-05 06:07:24 +0300

    updates: changes add getters

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -6,11 +6,18 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
-import static spark.Spark.get;
-import static spark.Spark.staticFileLocation;
+import static spark.Spark.*;
 
 public class App {
+    static int getHerokuAssignedPort() {
+        ProcessBuilder processBuilder = new ProcessBuilder();
+        if (processBuilder.environment().get("PORT") != null) {
+            return Integer.parseInt(processBuilder.environment().get("PORT"));
+        }
+        return 4567;
+    }
     public static void main(String[] args) {
+        port(getHerokuAssignedPort());
         staticFileLocation("/public");
         Hero.setUpFirstHero();
         Hero.setUpSecondHero();
